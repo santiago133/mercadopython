0f3655f81118d62611a531eeda1cf88589d6fafb
san1460 <165798237+san1460@users.noreply.github.com>
2025-02-26 10:23:42 -0500

    feat se cre menu deopciones

--- main.py
+++ main.py
@@ -2,4 +2,19 @@
 #  dejestion de mercado 
 nombreUsuario=None
 
+#listas 
+#productos =[]
+#producto={}
+
+print("***APP")
+print("agregar productos ")
+print ("ingrese con el 5")
+
+opsion=100
+while opsion != 5:
+    opsion = (int("digina una opcion del menu "))
+
+
+#crear un menud de opciones  
+
 
