88f8324e7c7ee7b2f4fd1381895b5fbc9f2968e9
san1460 <165798237+san1460@users.noreply.github.com>
2025-03-10 12:30:59 -0500

    continuar

--- main.py
+++ main.py
@@ -7,14 +7,33 @@ nombreUsuario=None
 #producto={}
 
 print("***APP")
-print("agregar productos ")
+print("1. agregar productos ")
+print("2. revisar tulista demercado ")
+print("3. modificar la lista del mercado")
+print("4. retirar el producto de tulista de metira ")
 print ("ingrese con el 5")
 
 opsion=100
 while opsion != 5:
-    opsion = (int("digina una opcion del menu "))
+    opsion = int(input("digina una opcion del menu "))
 
+    if opsion == 1:
+        print("ingresr lista ")
+    producto["id"]=5
+    producto["nombre "]=(input("ingresa e nombre del producto "))
+    producto["presentación"]=(input("Digite la presentacion del producto"))
+    producto["cantidad"]=(input("digite la cantidad del producto"))
+    producto["precio"]=(input("digite el precio "))
 
-#crear un menud de opciones  
+    productos.append(producto)
 
-
+    if opsion == 2:
+      print("mostrar lista  ")
+    elif opsion == 3 :
+        print("revisar la lista ")
+    elif opsion == 4:
+      print("modificar lista")
+    elif opsion == 5:
+      print("retirar producto")
+    else:
+        print("opciones invalida")
